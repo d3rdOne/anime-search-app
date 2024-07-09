f5b94a7c7d07732cb6d1b6609e9ef155b524c739
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-07-09 14:24:00 +0800

    Updated main styling from css to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 // import { useSelector } from "react-redux";
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {store} from './store/stores.js'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { store } from "./store/stores.js";
+import { Provider } from "react-redux";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
