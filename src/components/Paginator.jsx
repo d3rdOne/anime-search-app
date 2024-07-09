30dd3726a6f7661b3d7b1cea98a76dfa3bb30a79
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-07-09 14:26:46 +0800

    Add paginator styling

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -5,23 +5,19 @@ import { useSelector } from "react-redux";
 const Paginator = ({ containerClassName, onSelectPage }) => {
   const pageCount = useSelector((state) => state.animeSearch.pageCount);
   const currentPage = useSelector((state) => state.animeSearch.currentPage);
-  // const dispatch = useDispatch();
-  // const handlePageChange = (event) => {
-  //   // console.log(`changing page ${event.selected + 1}`);
 
-  // };
   return (
     <>
       <div className="w-full sm:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
         {pageCount > 0 && (
           <ReactPaginate
-            initialPage={currentPage - 1}
             pageCount={pageCount}
+            forcePage={currentPage - 1}
             pageRangeDisplayed={4}
             onPageChange={(event) => onSelectPage(event.selected)}
             nextLabel="NEXT"
             previousLabel="PREV"
-            containerClassName={`${containerClassName} w-full gap-0 sm:gap-2 flex flew-wrap justify-start text-xs sm:text-sm tracking-wider`}
+            containerClassName={`${containerClassName} w-full gap-0 sm:gap-2 flex flew-wrap justify-center text-xs sm:text-sm tracking-wider`}
             previousClassName=" rounded-sm border-[1px] py-1 px-2 border-black/50 bg-gray-50  hover:bg-gray-100 dark:bg-red-700 dark:border-gray-red dark:hover:bg-red-600"
             nextClassName="rounded-sm border-[1px] py-1 px-2 border-black/50 bg-gray-50 hover:bg-gray-100 dark:bg-red-700 dark:border-gray-red dark:hover:bg-red-600"
             pageClassName="rounded-sm border-[1px] py-1 px-2 border-black/50 bg-gray-50  hover:bg-gray-100 dark:bg-red-700 dark:border-gray-red dark:hover:bg-red-600"
