14b349969e1895fb949b169a03f9c8344e03fa44
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-07-09 14:26:19 +0800

    Update theme implementation

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -3,7 +3,7 @@ import { setTheme } from "../slice/animeSlice";
 import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
-const ThemeButton = ({ className }) => {
+const ThemeButton = () => {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.animeSearch.theme);
 
@@ -15,21 +15,26 @@ const ThemeButton = ({ className }) => {
     }
   }, [theme]);
 
-  const handleToggle = (checked) => {
-    dispatch(setTheme(checked ? "dark" : "light"));
+  const handleToggle = () => {
+    let newTheme = "";
+    switch (theme) {
+      case "dark":
+        newTheme = "light";
+        break;
+      case "light":
+        newTheme = "dark";
+        break;
+      default:
+        newTheme = "light";
+    }
+    dispatch(setTheme(newTheme));
   };
   return (
     <>
-      <label className={`${className} inline-flex items-center cursor-pointer`}>
-        <input
-          type="checkbox"
-          value={theme === "dark"}
-          className="sr-only peer"
-          onChange={(e) => handleToggle(e.target.checked)}
-        />
-        <div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-0 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
-        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-      </label>
+      <button className="toggle-button" onClick={handleToggle}>
+        <div className="sun"></div>
+        <div className="core"></div>
+      </button>
     </>
   );
 };
--- src/slice/animeSlice.js
+++ src/slice/animeSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   title: '',
-  theme: 'light',
+  theme: 'dark',
   currentPage: 1,
   pageCount: 0
 }
